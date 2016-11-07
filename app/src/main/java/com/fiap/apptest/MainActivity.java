caad5504565a500eba70d68c3ee55fd2cbb7de15
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-11-07 21:21:50 -0200

    Adding error messages and also changing from views

--- app/src/main/java/com/fiap/apptest/MainActivity.java
+++ app/src/main/java/com/fiap/apptest/MainActivity.java
@@ -1,19 +1,38 @@
 package com.fiap.apptest;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected TextInputLayout tilUsername;
+    protected TextInputLayout tilPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tilUsername = (TextInputLayout) findViewById(R.id.tilUser);
+        tilPassword = (TextInputLayout) findViewById(R.id.tilPassword);
     }
 
-    public void goTo(View v) {
-        Toast.makeText(this, "Cool", Toast.LENGTH_SHORT).show();
+    public void doLogin(View v) {
+        // reset error message
+        tilPassword.setErrorEnabled(false);
+
+        String user = tilUsername.getEditText().getText().toString();
+        String password = tilPassword.getEditText().getText().toString();
+
+        // isEmpty()
+        if (user.equals("caue") && password.equals("123")) {
+            Intent i = new Intent(this, FormActivity.class);
+            startActivity(i);
+        } else {
+            tilPassword.setError("Invalid username and/or password");
+        }
     }
 }
