1c76160b9406abcdcf1c27167b66f1acc83b2187
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-11-07 20:45:41 -0200

    Adding Password Field and Sign In button

--- app/src/main/java/com/fiap/apptest/MainActivity.java
+++ app/src/main/java/com/fiap/apptest/MainActivity.java
@@ -2,6 +2,8 @@ package com.fiap.apptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goTo(View v) {
+        Toast.makeText(this, "Cool", Toast.LENGTH_SHORT).show();
     }
 }
